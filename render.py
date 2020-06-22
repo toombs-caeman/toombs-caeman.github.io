18145849468452d4c1562e50dd275b502085080b
Caeman Toombs <toombs.caeman@gmail.com>
2020-06-22 13:15:47 -0400

    move development server to python

--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import markdown
 from os import path, walk, makedirs, remove
+
 from jinja2 import Environment, FileSystemLoader
 
 # CONFIG
@@ -20,8 +21,6 @@ SITE_CONFIG = {
 }
 
 # RENDER
-env = Environment(loader=FileSystemLoader(template_dir))
-md = markdown.Markdown(extensions=['meta'])
 
 
 def lsR(PATH):
@@ -29,41 +28,62 @@ def lsR(PATH):
         for f in filenames:
             yield path.join(dp, f)
 
+def render():
+    env = Environment(loader=FileSystemLoader(template_dir))
+    md = markdown.Markdown(extensions=['meta'])
 
-# delete old content
-for file in lsR(render_dir):
-    remove(file)
+    # delete old content
+    for file in lsR(render_dir):
+        remove(file)
 
-# create name mapping
-name_map = {}
-for file in lsR(content_dir):
-    sp = path.split(file)
-    new_file = path.join(render_dir, *sp[1:-1], sp[-1].replace(".md", ".html"))
-    name_map[file] = new_file
-    # make sure the directories exist
-    makedirs(
-        path.join(*path.split(new_file)[:-1]),
-        exist_ok=True,
-    )
+    # create name mapping
+    name_map = {}
+    for file in lsR(content_dir):
+        sp = path.split(file)
+        new_file = path.join(render_dir, *sp[1:-1], sp[-1].replace(".md", ".html"))
+        name_map[file] = new_file
+        # make sure the directories exist
+        makedirs(
+            path.join(*path.split(new_file)[:-1]),
+            exist_ok=True,
+        )
 
-# render markdown content
-for file, new_file in name_map.items():
-    with open(file, 'r') as source:
-        with open(new_file, 'w') as out:
-            print('converting', path.split(new_file)[-1])
-            html = md.convert(source.read())
-            meta = md.Meta
-            layout = meta.get("layout", ["default.html"])[0]
-            template = env.get_template(layout)
-            data = {
-                'site':SITE_CONFIG,
-                'page':meta,
-            }
-            out.write(
-                template.render(
-                    {
-                        'content':env.from_string(html).render(data),
-                        **data,
-                    }
+    # render markdown content
+    for file, new_file in name_map.items():
+        with open(file, 'r') as source:
+            with open(new_file, 'w') as out:
+                print('converting', path.split(new_file)[-1])
+                html = md.convert(source.read())
+                meta = md.Meta
+                layout = meta.get("layout", ["default.html"])[0]
+                template = env.get_template(layout)
+                data = {
+                    'site':SITE_CONFIG,
+                    'page':meta,
+                }
+                out.write(
+                    template.render(
+                        {
+                            'content':env.from_string(html).render(data),
+                            **data,
+                        }
+                    )
                 )
-            )
+
+
+
+
+def serve():
+    from flask import Flask, redirect
+    # set the project root directory as the static folder, you can set others.
+    app = Flask(__name__, static_url_path='', static_folder='')
+    app.route('/')(lambda :redirect('/index.html'))
+    app.run()
+
+if __name__ == "__main__":
+    import sys
+    render()
+    if len(sys.argv) > 1 and sys.argv[1] == 'serve':
+        serve()
+
+
